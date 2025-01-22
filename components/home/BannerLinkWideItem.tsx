5eb33eeb35fb05a8e57a1a5298ee89bdc4e0ec58
s.feli <samran.f.m@gmail.com>
2025-01-22 13:55:59 +0330

    fix style bugs

--- components/home/BannerLinkWideItem.tsx
+++ components/home/BannerLinkWideItem.tsx
@@ -19,12 +19,12 @@ const BannerLinkWideItem:React.FC<Props> = props => {
                 alt={props.title}
                 width={600}
                 height={300}
-                className="rounded-2xl w-full h-24 object-cover"
+                className="rounded-3xl w-full h-32 object-cover"
             />
-            <Link href={props.url} className="h-16 px-4 bg-[#1b2c3b] absolute bottom-0 left-0 flex justify-between gap-7 items-center rounded-bl-2xl rounded-tr-2xl">
+            <Link href={props.url} className="h-20 px-4 bg-[#1b2c3b] absolute bottom-0 left-0 flex justify-between gap-7 items-center rounded-bl-3xl rounded-tr-3xl">
 
-                <ClipRadius className="absolute bottom-full left-0 fill-[#1b2c3b] w-4 h-4" />
-                <ClipRadius className="absolute bottom-0 left-full fill-[#1b2c3b] w-4 h-4" />
+                <ClipRadius className="absolute bottom-full left-0 fill-[#1b2c3b] w-6 h-6" />
+                <ClipRadius className="absolute bottom-0 left-full fill-[#1b2c3b] w-6 h-6" />
 
                 <div className="w-20">
                     <strong className="font-semibold block text-sm truncate"> {props.title} </strong>
