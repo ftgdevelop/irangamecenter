6bf69dee20c62b9958d9e2821b1e0800f2eb1a6f
s.feli <samran.f.m@gmail.com>
2025-04-24 16:25:28 +0330

    save change

--- next.config.ts
+++ next.config.ts
@@ -12,10 +12,8 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   env: {
     PROJECT_SERVER_TYPE: "https://",
-    PROJECT_SERVER_IDENTITY2:"identity.irangamecenter.com",
-    PROJECT_SERVER_IDENTITY:"identity.safaraneh.com",
-    PROJECT_SERVER_PAYMENT2: "payline.samita.com",
-    PROJECT_SERVER_PAYMENT:"payline.safaraneh.com",
+    PROJECT_SERVER_IDENTITY:"identity.irangamecenter.com",
+    PROJECT_SERVER_PAYMENT: "payline.samita.com",
     PORT: '',
     PROJECT_SERVER_STRAPI:"strapi.irangamecenter.com",
     PROJECT_SERVER_STRAPI_TOKEN:"916f3f6237c66188b5a32bd37623c69fc5a5afdcd1042b6bb5279893b530cb36aaeae4130e195ee2b9ce34ac8f8668921494bbd72527eaeaf2e0c6f2451a2ab83cfdab4c46ebeb81e1e6932b2affd9a892dcef8262ec49450bb778e8d2a6c30c96baeb7d9119ca34ec646d318445dee014e1f32c6eb4806f48c2e2dca4d42189",
