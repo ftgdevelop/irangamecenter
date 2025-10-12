c5b35db9f21c9454e09ba3053137c87cf5ae5cd6
sepid <sepide.bri@gmail.com>
2025-10-12 21:59:53 +0330

    fix:ui of dots

--- components/products/ProductGalleryCarousel.tsx
+++ components/products/ProductGalleryCarousel.tsx
@@ -12,6 +12,7 @@ interface Props {
 
 const ProductGalleryCarousel: React.FC<Props> = ({ galleries = [] }) => {
   const playersRef = useRef<Map<string, HTMLVideoElement>>(new Map());
+  const dotsRef = useRef<HTMLUListElement | null>(null); 
 
   if (galleries.length === 0) {
     return (
@@ -25,29 +26,50 @@ const ProductGalleryCarousel: React.FC<Props> = ({ galleries = [] }) => {
     <div className="w-full mx-auto">
       <SlickSlider
         arrows={false}
-        customPaging={(i) => {
-          return (
-            <div>
-              <ProductGalleryThumbnail item={galleries[i]} />
-            </div>
-          );
-        }}
+        customPaging={(i) => (
+          <div>
+            <ProductGalleryThumbnail item={galleries[i]} />
+          </div>
+        )}
         dots={true}
-        dotsClass="overflow-x-scroll overflow-y-hidden !flex h-full gap-2 
-[&_.slick-active]:border [&_.slick-active]:border-white [&_.slick-active]:rounded-md"
+        dotsClass="!flex overflow-x-scroll scrollbar-hide gap-2
+[&>li:not(.slick-active)]:opacity-50
+[&>li.slick-active]:border [&>li.slick-active]:border-white [&>li.slick-active]:rounded-md"
         infinite
         speed={500}
         slidesToShow={1}
         slidesToScroll={1}
         rtl
+        autoplay
+        autoplaySpeed={4000}
+        beforeChange={(_, next) => {
+          const dotsContainer = dotsRef.current;
+          if (!dotsContainer) return;
+
+          const dots = dotsContainer.querySelectorAll<HTMLLIElement>("li");
+          const activeDot = dots[next];
+          if (activeDot) {
+            activeDot.scrollIntoView({
+              behavior: "smooth",
+              inline: "center",
+              block: "nearest",
+            });
+          }
+        }}
+        appendDots={(dots) => (
+          <ul
+            ref={dotsRef}
+            className="!flex overflow-x-scroll scrollbar-hide gap-2"
+          >
+            {dots}
+          </ul>
+        )}
       >
-        {galleries.map((item) => {
-          return (
-            <div key={item.id} className="relative h-64">
-              <ProductGalleryItem item={item} playersRef={playersRef} />
-            </div>
-          );
-        })}
+        {galleries.map((item) => (
+          <div key={item.id} className="relative h-64">
+            <ProductGalleryItem item={item} playersRef={playersRef} />
+          </div>
+        ))}
       </SlickSlider>
     </div>
   );
