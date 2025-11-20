30924087d804599af6875ccd4fe2eb41ad9d2a48
sepid <sepide.bri@gmail.com>
2025-11-20 13:44:47 +0330

    fix:remove redux persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/shared/layout/Layout";
-import { persistor, store } from "@/redux";
+import { store } from "@/redux"; 
 import '@mantine/core/styles.css';
 import '@/styles/rmdp.scss';
 import "@/styles/globals.scss";
@@ -7,35 +7,30 @@ import { createTheme, MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const theme = createTheme({
-    /** Put your mantine theme override here */
-  });
+  const theme = createTheme({});
 
   return (
     <MantineProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Head>
-            <title> Iran Game Center </title>
-            <meta name="robots" content="noindex,nofollow" />
+        <Head>
+          <title> Iran Game Center </title>
+          <meta name="robots" content="noindex,nofollow" />
 
-            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-            <link rel="shortcut icon" href="/favicon.ico" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="manifest" href="/site.webmanifest" />
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
 
-          </Head >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
 
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </PersistGate>
       </Provider>
     </MantineProvider>
-  )
+  );
 }
