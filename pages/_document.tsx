399231f7297bd45b29dff22dd858c0261337c557
s.feli <samran.f.m@gmail.com>
2025-12-16 19:49:17 +0330

    set google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="fa" dir="rtl">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N6VRBVVG');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}        
       </Head>
       <body className="antialiased font-iranyekan bg-white dark:bg-black" >
         
