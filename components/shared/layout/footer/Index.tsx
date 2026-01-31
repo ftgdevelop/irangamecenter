e969a386697f4b29574b09619554156d8748ba15
ftgdevelop <153296726+ftgdevelop@users.noreply.github.com>
2026-01-31 10:13:41 +0330

    Update Index.tsx

--- components/shared/layout/footer/Index.tsx
+++ components/shared/layout/footer/Index.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
-import Accordion from "../../Accordion";
 import Link from "next/link";
+import InfoCircleOutline from "@/components/icons/InfoCircleOutline";
+import Faq from "@/components/icons/Faq";
+import GameOutline from "@/components/icons/GameOutline";
+import Phone from "@/components/icons/Phone";
+import CaretLeft from "@/components/icons/CaretLeft";
 
 const Footer = () => {
 
@@ -31,15 +35,48 @@ const Footer = () => {
             }
         ];
 
+        const contactLinks = [        
+            {
+                url:"/about",
+                label:"درباره ما",
+                icon: <GameOutline className="w-5 h-5 fill-current" />
+            },
+            {
+                url:"/contact",
+                label:"تماس با ما",
+                icon: <Phone className="w-5 h-5 fill-none stroke-current" />
+            },
+            {
+                url:"/terms",
+                label:"قوانین و راهنما",
+                icon: <InfoCircleOutline className="w-5 h-5 fill-none stroke-current" />
+            },
+            {
+                url:"/faq",
+                label:"سوالات متداول",
+                icon: <Faq className="w-5 h-5 fill-none stroke-current" />
+            }
+        ]
+
     return (
         <footer className="p-3 px-5">
 
-            <Accordion
-                title="لینک های مهم"
-                content="لینک های مهم"
-                WrapperClassName={`border-b border-t border-neutral-300 dark:border-white/15 py-3`}
-                withArrowIcon
-            />
+            {contactLinks.map((item, index) => (
+                <Link 
+                    prefetch={false}
+                    key={item.label}
+                    href={item.url} 
+                    className={`flex justify-between items-center px-2 py-4 border-neutral-300 dark:border-white/15 text-sm ${index ? "border-t" : ""}`}
+                >
+                    <span className="flex gap-3 items-center">
+                        {item.icon}
+                        {item.label}
+                    </span>
+                    <CaretLeft className="w-4 h-4 fill-current" />
+                </Link>
+            ))}
+
+            
 
             <div className="py-5">
                 <strong className="block text-center font-semibold mb-4"> شبکه های اجتماعی </strong>
