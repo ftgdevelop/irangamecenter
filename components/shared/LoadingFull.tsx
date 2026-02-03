f1e524826f3aa8a6d152900436253ca282a89cc9
s.feli <samran.f.m@gmail.com>
2026-02-03 13:44:23 +0330

    fix light mode style bug

--- components/shared/LoadingFull.tsx
+++ components/shared/LoadingFull.tsx
@@ -26,7 +26,7 @@ const LoadingFull:React.FC<Props> = props => {
 
                 <div className={`bg-[#dddddd]/75 dark:bg-black/75 backdrop-blur-sm absolute top-0 left-0 w-full h-screen transition-all ${open ? "opacity-100" : "opacity-0"}`} />
 
-                <div className={`bg-white dark:bg-[#192a39] dark:text-white items-center p-5 max-w-80 w-10/12 justify-center flex flex-col rounded-2xl transition-all ${open ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className={`bg-white dark:bg-[#192a39] text-neutral-800 dark:text-white items-center p-5 max-w-80 w-10/12 justify-center flex flex-col rounded-2xl transition-all ${open ? "translate-x-0" : "-translate-x-full"}`}>
                     <Image src="/logo.svg" alt="irangamecenter" className="my-4 w-16 h-16" width={64} height={64} />
                     <div className="text-center">
                         <strong className="block text-lg font-bold">
