507f1dddc6e274cb388bdcade627b8bbbd1dca4a
s.feli <samran.f.m@gmail.com>
2026-02-06 09:13:09 +0330

    edit logo icons and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,6 +36,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <meta name="apple-mobile-web-app-title" content="Iran Game Center" />
           <link rel="manifest" href="/site.webmanifest" />
         </Head>
 
